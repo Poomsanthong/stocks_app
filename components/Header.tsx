339dcbeb4799c7e7197eb998381b97bcaf381f25
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-10-24 21:05:29 +0200

    feat: add layout and header components with navigation

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import Navitems from "./Navitems";
+import UserDropdown from "./UserDropdown";
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Siganlist logo"
+            width={140}
+            height={32}
+          />
+        </Link>
+        <nav className="hidden sm:block">
+          <Navitems />
+        </nav>
+        <UserDropdown />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAV_ITEMS: { href: string; label: string }[] = [
+  { href: "/", label: "Dashboard" },
+  { href: "/search", label: "search" },
+  { href: "/watchlist", label: "watchlist" },
+];
